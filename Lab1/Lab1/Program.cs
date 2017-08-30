f3a79abf006210a8fed1a456223d40116c121263
Mitch Talmadge <u1031378@utah.edu>
2017-08-30 11:21:45 -0600

    Initial Commit.

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point into the program.
+        /// </summary>
+        /// <param name="args">Arguments for the program. Not used.</param>
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Finally I got git to work");
+            Console.Read();
+
+        }
+    }
+}
