2b4e7ab56284b9cef71edfad6fba62657305581c
Mitch Talmadge <u1031378@utah.edu>
2017-09-01 23:39:44 -0600

    Added dummy variable evaluator (lookup) for tests, added helper method for asserting expression evaluations.

--- PS1/FormulaEvaluatorTests/EvaluatorTest.cs
+++ PS1/FormulaEvaluatorTests/EvaluatorTest.cs
@@ -11,14 +11,33 @@ namespace FormulaEvaluatorTests
     public class EvaluatorTest
     {
         /// <summary>
+        /// A dummy spreadsheet variable evaluator that uses the length of the
+        /// provided variable as the returned value.
+        /// </summary>
+        /// <param name="variable">The variable to evaluate.</param>
+        /// <returns>The length of the variable.</returns>
+        private int GetDummySpreadsheetValue(string variable)
+        {
+            return variable.Length;
+        }
+
+        /// <summary>
+        /// Helper method for asserting whether the expected result is returned from an evaluation of an expression.
+        /// </summary>
+        /// <param name="expectedResult">The expected result after evaluation.</param>
+        /// <param name="expression">The expression to evaluate.</param>
+        private void AssertEvaluation(int expectedResult, string expression)
+        {
+            Assert.AreEqual(expectedResult, Evaluator.Evaluate(expression, GetDummySpreadsheetValue));
+        }
+
+        /// <summary>
         /// Tests the evaluation of some simple expressions that do not contain any variables or edge cases.
         /// </summary>
         [TestMethod]
         public void TestEvaluationOfSimpleExpressions()
         {
-
-            Assert.AreEqual(27, Evaluator.Evaluate(@"(2 + 3) * 5 + 2", null));
-
+            AssertEvaluation(27, "(2 + 3) * 5 + 2");
         }
     }
 }
