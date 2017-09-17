54c74eb4a1c71132292f3f7a4d9bdc2044c68e82
Mitch Talmadge <u1031378@utah.edu>
2017-09-17 12:25:43 -0600

    Added tests for syntax errors.

---
+++ PS3/FormulaTester/FormulaTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetUtilities;
+
+namespace FormulaTester
+{
+    [TestClass]
+    public class FormulaTests
+    {
+        /// <summary>
+        /// Tests the syntax rule that there must be at least one token.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxAtLeastOneToken()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Formula(""));
+        }
+
+        /// <summary>
+        /// Tests the syntax rule that when reading tokens from left to right, at no point should the number of 
+        /// closing parentheses seen so far be greater than the number of opening parentheses seen so far.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxRightParentheses()
+        {
+            // Shouldn't throw
+            Assert.IsNotNull(new Formula("(10 + 5)"));
+            Assert.IsNotNull(new Formula("((10 + 5))"));
+
+            // Should throw
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(10 + 5))"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("((10 + 5)))"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("((10 + 5) + (5 + (5 * 10)) 10))"));
+        }
+
+        /// <summary>
+        /// Tests the syntax rule that the total number of opening parentheses must equal the total number of closing parentheses.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxBalancedParentheses()
+        {
+            // Shouldn't throw
+            Assert.IsNotNull(new Formula("(10 + 5)"));
+            Assert.IsNotNull(new Formula("((10 - 5))"));
+
+            // Should Throw
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(10 + 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("((10 / 5)"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(((10 - 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(((10 + 5) * (5 + (5 * 10)) 10)"));
+        }
+
+        /// <summary>
+        /// Tests the syntax rule that the first token of an expression must be a number, a variable, or an opening parenthesis.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxStartingToken()
+        {
+            // Shouldn't throw
+            Assert.IsNotNull(new Formula("(10 + 5)")); // Start with opening parenthesis
+            Assert.IsNotNull(new Formula("10 - 5")); // Start with number
+            Assert.IsNotNull(new Formula("A5 * 5")); // Start with variable
+
+            // Should Throw
+            Assert.ThrowsException<ArgumentException>(() => new Formula(")10 + 5("));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("_ 10 / 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("+ 5 * 10"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("!10 - 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("+(10 + 5) * 5"));
+        }
+
+        /// <summary>
+        /// Tests the syntax rule that the last token of an expression must be a number, a variable, or a closing parenthesis.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxLastToken()
+        {
+            // Shouldn't throw
+            Assert.IsNotNull(new Formula("(10 + 5)")); // End with closing parenthesis.
+            Assert.IsNotNull(new Formula("10 - 5")); // End with number.
+            Assert.IsNotNull(new Formula("5 * A5")); // End with variable.
+
+            // Should Throw
+            Assert.ThrowsException<ArgumentException>(() => new Formula(")10 - 5("));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 + 5 _"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("5 / 10 + "));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 - !5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("5 * (10 + 5)+"));
+        }
+
+        /// <summary>
+        /// Tests the syntax rule that any token that immediately follows an opening parenthesis 
+        /// or an operator must be either a number, a variable, or an opening parenthesis.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxParenthesisFollowing()
+        {
+            // Shouldn't throw
+            // Parenthesis
+            Assert.IsNotNull(new Formula("(10 + 5)")); // Number.
+            Assert.IsNotNull(new Formula("(A5 / 5)")); // Variable.
+            Assert.IsNotNull(new Formula("((10 * 5))")); // Opening Parenthesis.
+            Assert.IsNotNull(new Formula("((((((((((10 - 5))))))))))")); // Opening Parenthesis.
+            // Operator
+            Assert.IsNotNull(new Formula("10 + 5")); // Number.
+            Assert.IsNotNull(new Formula("10 * A5")); // Variable.
+            Assert.IsNotNull(new Formula("10 - (10 + 5)")); // Opening Parenthesis.
+
+            // Should Throw
+            // Parenthesis
+            Assert.ThrowsException<ArgumentException>(() => new Formula("()"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(_ + 5)"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(+ 5)"));
+
+            // Operator
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 + /"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 - *5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(10 * )"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 / )"));
+        }
+
+        /// <summary>
+        /// Tests the syntax rule that any token that immediately follows a number, a variable, 
+        /// or a closing parenthesis must be either an operator or a closing parenthesis.
+        /// </summary>
+        [TestMethod]
+        public void PublicTestSyntaxExtraFollowing()
+        {
+            // Shouldn't throw
+            // Number
+            Assert.IsNotNull(new Formula("(10 + 5)")); // Closing Parenthesis.
+            Assert.IsNotNull(new Formula("10 + 5")); // Operator.
+            Assert.IsNotNull(new Formula("10 - 5")); // Operator.
+            Assert.IsNotNull(new Formula("10 * 5")); // Operator.
+            Assert.IsNotNull(new Formula("10 / 5")); // Operator.
+            // Variable
+            Assert.IsNotNull(new Formula("(10 + A5)")); // Closing Parenthesis.
+            Assert.IsNotNull(new Formula("A5 + 10")); // Operator.
+            Assert.IsNotNull(new Formula("A5 - 10")); // Operator.
+            Assert.IsNotNull(new Formula("A5 * 10")); // Operator.
+            Assert.IsNotNull(new Formula("A5 / 10")); // Operator.
+            // Closing Parenthesis
+            Assert.IsNotNull(new Formula("((10 + 5))")); // Closing Parenthesis.
+            Assert.IsNotNull(new Formula("(5 + 5) + 10")); // Operator.
+            Assert.IsNotNull(new Formula("(5 / 5) - 10")); // Operator.
+            Assert.IsNotNull(new Formula("(5 / 5) * 10")); // Operator.
+            Assert.IsNotNull(new Formula("(5 / 5) / 10")); // Operator.
+
+            // Should Throw
+            // Number
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 _ 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 = 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("10 ... 5"));
+            // Variable
+            Assert.ThrowsException<ArgumentException>(() => new Formula("A5 _ 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("A5 = 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("A5 ... 5"));
+            // Closing Parenthesis
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(5 + 5) _ 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(5 + 5) = 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(5 + 5) ... 5"));
+            Assert.ThrowsException<ArgumentException>(() => new Formula("(5 + 5) (5 + 5)"));
+        }
+    }
+}
---
+++ PS3/FormulaTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FormulaTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FormulaTester")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d88a5608-f924-47a9-aad2-5b1a96bd97d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
