795c2c6f9dbb276b0c41cef70de300976a5714b4
Mitch Talmadge <u1031378@utah.edu>
2017-09-20 18:44:18 -0600

    Separated large CheckSyntax method into multiple helper methods.

--- PS3/Formula/ExpressionSyntaxChecker.cs
+++ PS3/Formula/ExpressionSyntaxChecker.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SpreadsheetUtilities.Utils;
+﻿using SpreadsheetUtilities.Utils;
+using Normalizer = System.Func<string, string>;
+using Validator = System.Func<string, bool>;
 
 namespace SpreadsheetUtilities
 {
@@ -13,7 +10,6 @@ namespace SpreadsheetUtilities
     /// <author>Mitch Talmadge, u1031378</author>
     internal static class ExpressionSyntaxChecker
     {
-
         /// <summary>
         /// Ensures that the provided expression can be evaluated without error.
         /// Throws a FormulaFormatException if there are any problems.
@@ -21,7 +17,7 @@ namespace SpreadsheetUtilities
         /// <param name="expression">The expression to check.</param>
         /// <param name="normalizer">The variable normalizer.</param>
         /// <param name="validator">The variable validator.</param>
-        internal static void CheckSyntax(string expression, Func<string, string> normalizer, Func<string, bool> validator)
+        internal static void CheckSyntax(string expression, Normalizer normalizer, Validator validator)
         {
             // Check for null or empty formulas.
             if (expression == null)
@@ -36,51 +32,24 @@ namespace SpreadsheetUtilities
             // The first token must be either an opening parenthesis, a number, or a variable.
             tokens.MoveNext();
             var token = tokens.Current;
-            if (!OperatorUtils.IsOpeningGroupOperator(token)
-                && !double.TryParse(token, out _)
-                && !(ExpressionUtils.IsVariable(token) && validator(normalizer(token))))
-                throw new FormulaFormatException(
-                    "The first token of the expression must be an opening parenthesis, a number, or a variable.");
+            CheckFirstToken(token, normalizer, validator);
 
             // Keep track of number of opening and closing parentheses.
             int numOpeningParentheses = 0, numClosingParentheses = 0;
 
-            // Keep track of the last token seen
-            string lastToken = null;
+            // Keep track of the previous token seen
+            string previousToken = null;
 
             // Iterate over each token in the enumerator.
             do
             {
                 token = tokens.Current;
 
-                if (lastToken != null)
-                {
-                    /* Check that any token immediately following an opening parenthesis or operator 
-                     * is either an opening parenthesis, a number, or a variable. */
-                    if (OperatorUtils.IsOpeningGroupOperator(lastToken)
-                        || OperatorUtils.IsArithmeticOperator(lastToken))
-                    {
-                        if (!OperatorUtils.IsOpeningGroupOperator(token)
-                            && !double.TryParse(token, out _)
-                            && !(ExpressionUtils.IsVariable(token) && validator(normalizer(token))))
-                            throw new FormulaFormatException(
-                                "Any tokens following an opening parenthesis or operator must be an opening parenthesis, a number, or a variable.\n" +
-                                $"The token '{token}' is incorrectly following the token '{lastToken}'");
-                    }
+                // Check tokens which follow opening parentheses or operators.
+                CheckFollowingOpeningParenthesesOperators(previousToken, token, normalizer, validator);
 
-                    /* Check that any token immediately following a number, a variable, or a closing parenthesis 
-                     * is either an operator or a closing parenthesis */
-                    if (OperatorUtils.IsClosingGroupOperator(lastToken)
-                        || double.TryParse(lastToken, out _)
-                        || ExpressionUtils.IsVariable(lastToken) && validator(normalizer(lastToken)))
-                    {
-                        if (!OperatorUtils.IsClosingGroupOperator(token)
-                            && !OperatorUtils.IsArithmeticOperator(token))
-                            throw new FormulaFormatException(
-                                "Any tokens following a closing parenthesis, number, or variable must be a closing parenthesis or operator.\n" +
-                                $"The token '{token}' is incorrectly following the token '{lastToken}'");
-                    }
-                }
+                // Check tokens which follow closing parentheses, numbers, or variables.
+                CheckFollowingClosingParenthesesNumbersVariables(previousToken, token, normalizer, validator);
 
                 // Check parentheses.
                 if (OperatorUtils.IsOpeningGroupOperator(token))
@@ -99,18 +68,13 @@ namespace SpreadsheetUtilities
                             "There are too many closing parentheses in the expression.");
                 }
 
-                lastToken = token;
+                previousToken = token;
             } while (tokens.MoveNext());
             // Dispose of tokens enumerator.
             tokens.Dispose();
 
-            // Check that the last token is legal.
-            // The last token must be a closing parenthesis, a number, or a variable.
-            if (!OperatorUtils.IsClosingGroupOperator(token)
-                && !double.TryParse(token, out _)
-                && !(ExpressionUtils.IsVariable(token) && validator(normalizer(token))))
-                throw new FormulaFormatException(
-                    "The last token of the expression must be a closing parenthesis, a number, or a variable.");
+            // Check the final token.
+            CheckFinalToken(token, normalizer, validator);
 
             // Make sure the parentheses are balanced (opening == closed)
             if (numOpeningParentheses != numClosingParentheses)
@@ -118,5 +82,100 @@ namespace SpreadsheetUtilities
                     "Parentheses are unbalanced; the number of opening parentheses does not match the number of closing parentheses.");
         }
 
+        /// <summary>
+        /// Ensures that the final token of an expression is an opening parenthesis, a number, or a variable.
+        /// Throws if not legal.
+        /// </summary>
+        /// <param name="token">The final token of the expression.</param>
+        /// <param name="normalizer">The normalizer.</param>
+        /// <param name="validator">The validator.</param>
+        private static void CheckFirstToken(string token, Normalizer normalizer, Validator validator)
+        {
+            if (!OperatorUtils.IsOpeningGroupOperator(token)
+                && !double.TryParse(token, out _)
+                && !(ExpressionUtils.IsVariable(token) && validator(normalizer(token))))
+                throw new FormulaFormatException(
+                    "The first token of the expression must be an opening parenthesis, a number, or a variable.");
+        }
+
+        /// <summary>
+        /// Ensures that the final token of an expression is a closing parenthesis, a number, or a variable.
+        /// Throws if not legal.
+        /// </summary>
+        /// <param name="token">The final token of the expression.</param>
+        /// <param name="normalizer">The normalizer.</param>
+        /// <param name="validator">The validator.</param>
+        private static void CheckFinalToken(string token, Normalizer normalizer, Validator validator)
+        {
+            if (!OperatorUtils.IsClosingGroupOperator(token)
+                && !double.TryParse(token, out _)
+                && !(ExpressionUtils.IsVariable(token) && validator(normalizer(token))))
+                throw new FormulaFormatException(
+                    "The last token of the expression must be a closing parenthesis, a number, or a variable.");
+        }
+
+        /// <summary>
+        /// Ensures that any token immediately following an opening parenthesis or operator
+        /// is either another opening parenthesis, a number, or a variable.
+        /// </summary>
+        /// <param name="previousToken">The previous token iterated over. Null if this is the first.</param>
+        /// <param name="currentToken">The current token being checked.</param>
+        /// <param name="normalizer">The normalizer.</param>
+        /// <param name="validator">The validator.</param>
+        private static void CheckFollowingOpeningParenthesesOperators(
+            string previousToken,
+            string currentToken,
+            Normalizer normalizer,
+            Validator validator)
+        {
+            // Nothing to check if previous is null.
+            if (previousToken == null)
+                return;
+
+            // Make sure the previous token was either an opening group operator or an arithmetic operator.
+            if (!OperatorUtils.IsOpeningGroupOperator(previousToken)
+                && !OperatorUtils.IsArithmeticOperator(previousToken))
+                return;
+
+            // Make sure the current token is either an opening group operator, a number, or a variable.
+            if (!OperatorUtils.IsOpeningGroupOperator(currentToken)
+                && !double.TryParse(currentToken, out _)
+                && !(ExpressionUtils.IsVariable(currentToken) && validator(normalizer(currentToken))))
+                throw new FormulaFormatException(
+                    "Any tokens following an opening parenthesis or operator must be an opening parenthesis, a number, or a variable.\n" +
+                    $"The token '{currentToken}' is incorrectly following the token '{previousToken}'");
+        }
+
+        /// <summary>
+        /// Ensures that any token immediately following a closing parenthesis, number, or variable
+        /// is either another closing parenthesis or an operator.
+        /// </summary>
+        /// <param name="previousToken">The previous token iterated over. Null if this is the first.</param>
+        /// <param name="currentToken">The current token being checked.</param>
+        /// <param name="normalizer">The normalizer.</param>
+        /// <param name="validator">The validator.</param>
+        private static void CheckFollowingClosingParenthesesNumbersVariables(
+            string previousToken,
+            string currentToken,
+            Normalizer normalizer,
+            Validator validator)
+        {
+            // Nothing to check if previous is null.
+            if (previousToken == null)
+                return;
+
+            // Make sure the previous token was either a closing group operator, a number, or a variable.
+            if (!OperatorUtils.IsClosingGroupOperator(previousToken)
+                && !double.TryParse(previousToken, out _)
+                && (!ExpressionUtils.IsVariable(previousToken) || !validator(normalizer(previousToken))))
+                return;
+
+            // Make sure the current token is either a closing group operator or an arithmetic operator.
+            if (!OperatorUtils.IsClosingGroupOperator(currentToken)
+                && !OperatorUtils.IsArithmeticOperator(currentToken))
+                throw new FormulaFormatException(
+                    "Any tokens following a closing parenthesis, number, or variable must be a closing parenthesis or operator.\n" +
+                    $"The token '{currentToken}' is incorrectly following the token '{previousToken}'");
+        }
     }
 }
