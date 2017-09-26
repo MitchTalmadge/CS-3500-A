50527b837a8684435de2d16b0f5534ecba9da58d
Mitch Talmadge <u1031378@utah.edu>
2017-09-26 01:04:21 -0600

    Added skeleton code for PS4

--- PS4/Spreadsheet/Spreadsheet.cs
+++ PS4/Spreadsheet/Spreadsheet.cs
@@ -1,7 +1,38 @@
-﻿namespace SS
-{
-    public class Spreadsheet
-    {
+﻿using System.Collections.Generic;
+using SpreadsheetUtilities;
 
+namespace SS
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override object GetCellContents(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
