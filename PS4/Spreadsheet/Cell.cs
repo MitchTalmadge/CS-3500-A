fedbc01b56debb1105b5d4fa5868dc65942acf3e
Mitch Talmadge <u1031378@utah.edu>
2017-09-29 15:08:05 -0600

    Commented cell class

--- PS4/Spreadsheet/Cell.cs
+++ PS4/Spreadsheet/Cell.cs
@@ -12,11 +12,22 @@ namespace SS
     /// <author>Mitch Talmadge, u1031378</author>
     internal class Cell
     {
+        /// <summary>
+        /// Constructs a Cell with the given contents.
+        /// </summary>
+        /// <param name="content">
+        /// The original contents of the Cell (not evaluated).
+        /// Should be either a double, string, or Formula.
+        /// </param>
         public Cell(object content)
         {
             Content = content;
         }
 
+        /// <summary>
+        /// Represents the original, non-evaluated content of this Cell.
+        /// May be a double, string, or Formula.
+        /// </summary>
         internal object Content { get; }
     }
 }
